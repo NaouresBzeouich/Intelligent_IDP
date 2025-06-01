f573fa2d2f51a3dac80cc68cc0c09cdb82c86fd6
Naoures <naoures.bzeouich@insat.ucar.tn>
2025-06-01 09:51:39 +0100

    azure template
    the service is well done , but there's a prob I will fix it on the server created by azure (the template tf itself)

--- backend/services/terraform.py
+++ backend/services/terraform.py
@@ -7,7 +7,7 @@ load_dotenv()
 key_name = os.getenv("TERRAFORM_PUB_KEY")
 
 # Set up Jinja2 environment to load templates from current dir
-env = Environment(loader=FileSystemLoader(searchpath="../templates/terraform"))
+env = Environment(loader=FileSystemLoader(searchpath="./backend/templates/terraform"))
 
 instance_name='testingg' #####################################
 
@@ -29,5 +29,23 @@ def create_tf_aws(output_dir,instance_name):
 
     print(f"Generated Terraform file at {output_file_path}")
 
-# Example usage:
-create_tf_aws("terraform_clients/client1",instance_name)
+def create_tf_azure(output_dir, instance_name):
+    # Load your template
+    template = env.get_template("azure.tf.j2")
+
+    output_from_template = template.render(key_name=key_name, instance_name=instance_name)
+    
+    # Ensure the output directory exists, create if not
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Path where the file will be created
+    output_file_path = os.path.join(output_dir, "main.tf")
+
+    # Write rendered template to the output path
+    with open(output_file_path, "w") as fh:
+        fh.write(output_from_template)
+
+    print(f"Generated Azure Terraform file at {output_file_path}")
+
+
+create_tf_azure("terraform_clients/client1_azure", instance_name)
